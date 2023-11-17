4c96e7c2bdac05596da2bf24d685cd5d0b9da331
orcchieftain <karlos.slaj@gmail.com>
2023-11-17 19:33:31 +0100

    Lecture 4 converted. L 5 and L 6 will be done this Saturday/Sunday respectively. However, I didn't do refactoring and won't.

---
+++ 5. Buildings And Trees/src/primitives/polygon.ts
@@ -0,0 +1,185 @@
+import type { PointClass } from "../types/primitiveTypes";
+import type { SegmentClass } from "../types/primitiveTypes";
+import Segment from "./segment";
+import Point from "./point";
+import { getIntersection, average, getRandomColor } from "../math/utils";
+
+/**
+ * Class representing a polygon shape.
+ * HERE
+ * HERE
+ * HERE METHODS
+ * HERE PROPS
+ * METHODS
+ * PROPS
+ * ! should document better
+ */
+class Polygon {
+  /**
+   * Array of points defining the vertices of the polygon.
+   * @type {PointClass[]}
+   */
+  points: PointClass[];
+  /**
+   * Array of segments connecting the points of the polygon.
+   * @type {SegmentClass[]}
+   */
+  segments: SegmentClass[];
+
+  /**
+   * Creates a Polygon instance.
+   * @param {PointClass[]} points - Array of points to define the polygon vertices. Could theoretically be done with Mathpoints if some methods were independent of the Point class.
+   */
+  constructor(points: PointClass[]) {
+    this.points = points;
+    this.segments = [];
+    for (let i = 1; i <= points.length; i++) {
+      this.segments.push(new Segment(points[i - 1], points[i % points.length]));
+    }
+  }
+
+  /**
+   * Draws the polygon on a canvas context.
+   * @param {CanvasRenderingContext2D} ctx - The canvas rendering context.
+   * @param {Object} [style] - Optional styling options for the polygon.
+   * ! should also define type for the drawing options when the project nears its end
+   */
+  draw(ctx: CanvasRenderingContext2D, { stroke = "blue", linewidth = 2, fill = "rgba(0,0,255,0.3)" } = {}) {
+    ctx.beginPath();
+    ctx.fillStyle = fill;
+    ctx.strokeStyle = stroke;
+    ctx.lineWidth = linewidth; // Note: It should be `lineWidth`, not `linewidth`
+    ctx.moveTo(this.points[0].x, this.points[0].y);
+    for (let i = 1; i < this.points.length; i++) {
+      ctx.lineTo(this.points[i].x, this.points[i].y);
+    }
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+  }
+
+  /**
+   * Static method to break two polygons at their intersections.
+   * @param {Polygon} poly1 - First polygon.
+   * @param {Polygon} poly2 - Second polygon.
+   */
+  static break(poly1: Polygon, poly2: Polygon) {
+    const segs1 = poly1.segments;
+    const segs2 = poly2.segments;
+    for (let i = 0; i < segs1.length; i++) {
+      for (let j = 0; j < segs2.length; j++) {
+        const int = getIntersection(segs1[i].p1, segs1[i].p2, segs2[j].p1, segs2[j].p2);
+        if (int && int.offset != 1 && int.offset != 0) {
+          const point = new Point(int.x, int.y);
+          let aux = segs1[i].p2;
+          segs1[i].p2 = point;
+          segs1.splice(i + 1, 0, new Segment(point, aux));
+          aux = segs2[j].p2;
+          segs2[j].p2 = point;
+          segs2.splice(j + 1, 0, new Segment(point, aux));
+        }
+      }
+    }
+  }
+
+  /**
+   * Static method to break multiple polygons at their intersections.
+   * @param {Polygon[]} polys - Array of polygons.
+   */
+  static multiBreak(polys: Polygon[]) {
+    for (let i = 0; i < polys.length - 1; i++) {
+      for (let j = i + 1; j < polys.length; j++) {
+        Polygon.break(polys[i], polys[j]);
+      }
+    }
+  }
+
+  /**
+   * Static method to calculate the union of multiple polygons.
+   * @param {Polygon[]} polys - Array of polygons.
+   * @returns {SegmentClass[]} - Array of segments representing the union.
+   */
+  static union(polys: Polygon[]) {
+    Polygon.multiBreak(polys);
+    const keptSegments = [];
+    for (let i = 0; i < polys.length; i++) {
+      for (const seg of polys[i].segments) {
+        let keep = true;
+        for (let j = 0; j < polys.length; j++) {
+          if (i != j) {
+            if (polys[j].containsSegment(seg)) {
+              keep = false;
+              break;
+            }
+          }
+        }
+        if (keep) {
+          keptSegments.push(seg);
+        }
+      }
+    }
+    return keptSegments;
+  }
+
+  /**
+   * Checks if the polygon contains a given segment.
+   * @param {SegmentClass} seg - The segment to check.
+   * @returns {boolean} - True if the polygon contains the segment, otherwise false.
+   */
+  containsSegment(seg: SegmentClass) {
+    const midpoint = average(seg.p1, seg.p2);
+    return this.containsPoint(midpoint);
+  }
+
+  /**
+   * Checks if the polygon contains a given point.
+   * @param {PointClass | void} point - The point to check.
+   * @returns {boolean} - True if the polygon contains the point, otherwise false.
+   */
+  containsPoint(point: PointClass | void) {
+    if (!point) {
+      return 0;
+    }
+    const outerPoint = new Point(-1000, -1000);
+    let intersectionsCount = 0;
+    for (const seg of this.segments) {
+      const int = getIntersection(outerPoint, point, seg.p1, seg.p2);
+
+      if (int) {
+        intersectionsCount++;
+      }
+    }
+    return intersectionsCount % 2 == 1;
+  }
+
+  intersectsPoly(poly: Polygon) {
+    for (let s1 of this.segments) {
+      for (let s2 of poly.segments) {
+        if (getIntersection(s1.p1, s1.p2, s2.p1, s2.p2)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Draws all segments of the polygon with random colors.
+   * @param {CanvasRenderingContext2D} ctx - The canvas rendering context.
+   */
+  drawSegments(ctx: CanvasRenderingContext2D) {
+    for (const seg of this.segments) {
+      seg.draw(ctx, { color: getRandomColor(), lineWidth: 5 });
+    }
+  }
+
+  distanceToPoint(point: PointClass) {
+    return Math.min(...this.segments.map((s) => s.distaneToPoint(point)));
+  }
+
+  distanceToPoly(poly: Polygon) {
+    return Math.min(...this.points.map((p) => poly.distanceToPoint(p)));
+  }
+}
+
+export default Polygon;
---
+++ 5. Buildings And Trees/src/world.ts
@@ -0,0 +1,252 @@
+import Envelope from "./primitives/envelope";
+import Polygon from "./primitives/polygon";
+import Segment from "./primitives/segment";
+import Point from "./primitives/point";
+import type { GraphType } from "./types/mathTypes";
+import { PointClass, SegmentClass } from "./types/primitiveTypes";
+import { add, scale, distance, lerp } from "./math/utils";
+
+class World {
+  /**
+   * A graph structure representing the layout of the world.
+   * @type {GraphType}
+   */
+  graph: GraphType;
+  /**
+   * The width of the roads in the world.
+   * @type {number}
+   */
+  roadWidth: number;
+  /**
+   * The roundness of the roads in the world.
+   * @type {number}
+   */
+  roadRoundness: number;
+  /**
+   * An array of Envelope objects representing areas around the road segments.
+   * @type {Envelope[]}
+   */
+  envelopes: Envelope[];
+  /**
+   * An array of SegmentClass objects representing the borders of the road.
+   * It can be undefined if not initialized.
+   * @type {SegmentClass[] | undefined}
+   */
+  roadBorders: SegmentClass[] | undefined; // ! not sure if undefined is needed here
+  /**
+   * Represents intersections in the world.
+   * It can be undefined if not initialized.
+   * @type {PointClass | undefined}
+   */
+  intersections: PointClass | undefined;
+  /**
+   * The width of the buildings in the world.
+   * @type {number}
+   */
+  buildingWidth: number;
+  /**
+   * The minimum length of the buildings in the world.
+   * @type {number}
+   */
+  buildingMinLength: number;
+  /**
+   * The spacing between buildings and trees in the world.
+   * @type {number}
+   */
+  spacing: number;
+  /**
+   *
+   */
+  treeSize: number;
+  /**
+   *
+   */
+  buildings: any[]; // ! not sure what type this should be as of now
+  /**
+   *
+   */
+  trees: any[]; // ! not sure what type this should be as of now
+
+  /**
+   * Constructor to create a World instance.
+   * @param {GraphType} graph - The graph structure of the world.
+   * @param {number} [roadWidth=100] - The default width of the roads.
+   * @param {number} [roadRoundness=3] - The default roundness of the roads.
+   */
+  constructor(
+    graph: GraphType,
+    roadWidth: number = 100,
+    roadRoundness: number = 10,
+    buildingWidth: number = 150,
+    buildingMinLength = 150,
+    spacing = 50
+  ) {
+    this.graph = graph;
+    this.roadWidth = roadWidth;
+    this.roadRoundness = roadRoundness;
+    this.buildingWidth = buildingWidth;
+    this.buildingMinLength = buildingMinLength;
+    this.spacing = spacing;
+    this.treeSize = 200;
+    this.envelopes = [];
+    this.roadBorders = [];
+    this.buildings = [];
+    this.trees = [];
+
+    this.generate();
+  }
+  // ! the problem is that with lower number it may be impossible to place all of the tries and this will never end
+  private generateTrees(count: number = 120): PointClass[] {
+    if (!this.roadBorders) {
+      return [];
+    }
+
+    const points = [...this.roadBorders.map((s) => [s.p1, s.p2]).flat(), ...this.buildings.map((b) => b.points).flat()];
+
+    const left = Math.min(...points.map((p) => p.x));
+    const right = Math.max(...points.map((p) => p.x));
+    const top = Math.min(...points.map((p) => p.y));
+    const bottom = Math.max(...points.map((p) => p.y));
+
+    const illegalPolys = [...this.buildings, ...this.envelopes.map((e) => e.poly)];
+
+    const trees = [];
+    let tryCount = 0;
+    while (trees.length < count && tryCount < 100) {
+      const p = new Point(lerp(left, right, Math.random()), lerp(top, bottom, Math.random()));
+
+      let keep = true;
+
+      //intersections with roads
+      for (const poly of illegalPolys) {
+        if (poly.containsPoint(p) || poly.distanceToPoint(p) < this.treeSize / 2) {
+          keep = false;
+          break;
+        }
+      }
+
+      //intersections with other trees
+      // ! optional
+      if (keep) {
+        for (const tree of trees) {
+          if (distance(tree, p) < this.treeSize) {
+            keep = false;
+            break;
+          }
+        }
+      }
+
+      //preventing generation of trees too far away from the roads
+      if (keep) {
+        let closeToSomething = false;
+        for (const poly of illegalPolys) {
+          if (poly.distanceToPoint(p) < this.treeSize * 2) {
+            closeToSomething = true;
+            break;
+          }
+        }
+        keep = closeToSomething;
+      }
+
+      if (keep) {
+        trees.push(p);
+        tryCount = 0;
+      }
+      tryCount++;
+    }
+    return trees;
+  }
+  //!any
+  private generateBuildings(): any {
+    const tmpEnvelopes = [];
+    for (const seg of this.graph.segments) {
+      tmpEnvelopes.push(new Envelope(seg, this.roadWidth + this.buildingWidth + this.spacing * 2, this.roadRoundness));
+    }
+
+    const guides = Polygon.union(tmpEnvelopes.map((e) => e.poly));
+
+    for (let i = 0; i < guides.length; i++) {
+      const seg = guides[i];
+
+      if (seg.length() < this.buildingMinLength) {
+        guides.splice(i, 1);
+        i--;
+      }
+    }
+
+    const supports = [];
+    for (let seg of guides) {
+      const len = seg.length() + this.spacing;
+      const buildingCount = Math.floor(len / (this.buildingMinLength + this.spacing));
+      const buildingLength = len / buildingCount - this.spacing;
+      const dir = seg.directionVector();
+
+      let q1 = seg.p1;
+      let q2 = add(q1, scale(dir, buildingLength));
+      supports.push(new Segment(q1, q2));
+
+      for (let i = 2; i <= buildingCount; i++) {
+        q1 = add(q2, scale(dir, this.spacing));
+        q2 = add(q1, scale(dir, buildingLength));
+        supports.push(new Segment(q1, q2));
+      }
+    }
+
+    const bases = [];
+    for (const seg of supports) {
+      bases.push(new Envelope(seg, this.buildingWidth).poly);
+    }
+
+    const eps = 0.001;
+    for (let i = 0; i < bases.length - 1; i++) {
+      for (let j = i + 1; j < bases.length; j++) {
+        // second condition prevents buildings on different segments spawning on top
+        if (bases[i].intersectsPoly(bases[j]) || bases[i].distanceToPoly(bases[j]) < this.spacing - eps) {
+          bases.splice(j, 1);
+          j--;
+        }
+      }
+    }
+
+    return bases;
+  }
+
+  /**
+   * Generates the envelopes and road borders based on the graph segments.
+   */
+  generate(): void {
+    this.envelopes.length = 0;
+    for (const seg of this.graph.segments) {
+      this.envelopes.push(new Envelope(seg, this.roadWidth, this.roadRoundness));
+    }
+    this.roadBorders = Polygon.union(this.envelopes.map((e) => e.poly));
+    this.buildings = this.generateBuildings();
+    this.trees = this.generateTrees();
+  }
+
+  /**
+   * Draws the world on a canvas.
+   * @param {CanvasRenderingContext2D} ctx - The canvas rendering context.
+   */
+  draw(ctx: CanvasRenderingContext2D): void {
+    for (const env of this.envelopes) {
+      env.draw(ctx, { fill: "#BBB", stroke: "#BBB", lineWidth: 15 });
+    }
+    for (const seg of this.graph.segments) {
+      seg.draw(ctx, { color: "white", lineWidth: 4, dash: [10, 10] });
+    }
+    if (this.roadBorders) {
+      for (const seg of this.roadBorders) {
+        seg.draw(ctx, { color: "red", lineWidth: 2 });
+      }
+    }
+    for (const tree of this.trees) {
+      tree.draw(ctx, { size: this.treeSize, fill: "green" });
+    }
+    for (const bld of this.buildings) {
+      bld.draw(ctx);
+    }
+  }
+}
+
+export default World;
